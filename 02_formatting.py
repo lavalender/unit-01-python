7d285efe03835b61faed1bd2a0e829b97d79aff9
Valerie Burke <vburke@brooklynsteamcenter.org>
2025-09-16 10:37:19 -0400

    task2complete

--- 02_formatting.py
+++ 02_formatting.py
@@ -5,19 +5,19 @@ The password should not be case sensitive
 
 """
 
-ask = input("Enter the password: ")
-if ask.lower() == "purpledolphin":
+ask = input("Enter the password: ") # first i asked for the user to input a password
+if ask.lower() == "purpledolphin": # i put in the password that would work for my code and set it to automatically be lower after they put the password 
     print("You got it") 
 else:
-    print("WRONG")
+    print("WRONG") #if they didnt type in the right password it was wrong but as long as its the right password it shoudl be good
 
 """
 TASK 2:
 Write code that checks if a user inputs an empty string
 If the string is empty, print "invalid" otherwise print "valid"
 """
-ask = input("Favorite Animal: ")
-if ask.strip() == "":
+ask = input("Favorite Animal: ") # i did this to gain an inout 
+if ask.strip() == "": #strip helps to remove the spaces and then the quotes check if the input is blank
     print("Wrong")
 else:
     print("Right")
@@ -28,8 +28,8 @@ TASK 3:
 Write a program that will replace the word "cat" with the word "dog"
 It should replace all occurances regardless of captilization 
 """
-animals= input("Which do you like most cat or dog: ")
-animal = animals.lower().replace("cat", "dog")
+animals= input("Which do you like most cat or dog: ") # To gain an input between the two i asked this question 
+animal = animals.lower().replace("cat", "dog") # if they put cat despite if it is uppercase it will be set to lower and then set it to dog
 print(animal)
 
 
@@ -38,9 +38,9 @@ TASK 4:
 
 Write a program that takes a person's name and age as input and prints it
 """
-name = input("What's your name? ")
-age = input("What's your age?")
-print(name + " you're " + age + " years old. ")
+name = input("What's your name? ") #ask for a name input
+age = input("What's your age? ") #ask for an age input
+print(name + " you're " + age + " years old. ") #print both responses into a sentence
 
 """
 TASK 5:
@@ -48,7 +48,8 @@ TASK 5:
 Write a program that takes in two floats, and prints the quotient
 The result should be rounded to the nearest tenth (1 decimal place)
 """
-float1 = input("Put the first float: ")
-float2 = input("Put the second float: ")
-result = float2 / float1 
-print(result)
+float1 = float(input("Put the first float: ")) #I asked for a float and if they didnt put a float in then it would automatically change it to one
+float2 = float(input("Put the second float: ")) # i did it twice to get two floats
+result = float2 / float1 #I divded the two floats
+rounded_result = round(result, 1) #this rounds the result from the floats to the first tenth place
+print(rounded_result) #lastly prints that new rounded result
---
+++ 03_list_blitz.py
@@ -0,0 +1,47 @@
+"""
+Task 1: Create a list
+Create a list with 4 elements and print it.
+"""
+
+"""
+Task 2: Add Element to a List
+Add an element to the end of the list. Print the updated 
+list.
+"""
+
+"""
+Task 3: Remove Element from a List
+Remove a specific element from the list. Print the 
+updated list.
+"""
+
+"""
+Task 4: Modify Element in a List
+Modify an element at a specific index with a new value. 
+Print the updated list.
+"""
+
+"""
+Task 5: Append Multiple Elements to a List
+Append multiple elements to the end of the list. Print 
+the updated list.
+"""
+
+"""
+Task 6: Delete Element at a Specific Index
+Delete an element at a specific index. Print the updated 
+list.
+"""
+
+
+"""
+Task 7: Slicing lists
+Create a new variable equal to the first 2 items of your list
+Then print out the new variable
+"""
+
+"""
+Task 8: Extend a List
+Extend the list with the elements of another list. Print 
+the updated list.
+"""
